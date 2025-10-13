7abf2fd748e070911cc9b70fcad66740b6315ffb
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2025-10-13 22:08:37 +0300

    Add include guards to git.h

--- stirc/bootstrapnomake.sh
+++ stirc/bootstrapnomake.sh
@@ -13,12 +13,15 @@ CFLAGS="-O3 -Wall -g"
 
 libobjs=""
 
-echo "static const char *gitshas[] = {" > git.h
+echo "#ifndef _GIT_H_" > git.h
+echo "#define _GIT_H_" >> git.h
+echo "static const char *gitshas[] = {" >> git.h
 git log --pretty=format:\"%h\", --abbrev=40 >> git.h
 echo "};" >> git.h
 echo -n "static const char *gitversion = \"" >> git.h
 echo -n `git describe --tags|sed 's/^v//g'` >> git.h
 echo "\";" >> git.h
+echo "#endif" >> git.h
 
 for a in *.l; do
   base="`echo "$a"|sed 's/.l$//g'`"
