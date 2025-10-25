af7da9a9f9ec63e7594c0dac5660fe465e6d56a4
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2025-10-25 13:31:48 +0300

    Package script

---
+++ package.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+die()
+{
+  echo "$@"
+  exit 1
+}
+
+tmpdir="`mktemp -d`"
+pwd="`pwd`"
+cd "$tmpdir" || die "Can't goto tmp dir"
+git clone --recursive https://github.com/Aalto5G/stirmake || die "Can't clone"
+cd stirmake/stirc || die "Can't chdir"
+smka git.h || die "Can't create git.h"
+rm .stir.db || die "Can't remove .stir.db"
+vname="`git describe --tags`"
+rm -rf ../.git
+rm -rf abce/.git
+cd ../.. || die "Can't chdir"
+mv stirmake stirmake-"$vname" || die "Can't rename dirctory"
+tar czvf stirmake-"$vname".tar.gz stirmake-"$vname" || die "Can't create tar.gz"
+rm -rf stirmake-"$vname"
+cd "$pwd" || die "Can't chdir back to old directory"
+if [ -e "stirmake-$vname.tar.gz" ]; then
+  die "Package stirmake-$vname.tar.gz already exists"
+else
+  mv "$tmpdir/stirmake-$vname.tar.gz" . || die "Can't move package"
+fi
+rmdir "$tmpdir" || die "Can't rm temporary dir"
+echo
+echo "Package created:"
+echo "stirmake-$vname.tar.gz"
