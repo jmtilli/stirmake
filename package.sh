4613e8858105017a89169a8bf45701b821e57e4b
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2025-10-25 13:36:12 +0300

    These are unnecessary

--- package.sh
+++ package.sh
@@ -13,6 +13,10 @@ git clone --recursive https://github.com/Aalto5G/stirmake || die "Can't clone"
 cd stirmake/stirc || die "Can't chdir"
 smka git.h || die "Can't create git.h"
 rm .stir.db || die "Can't remove .stir.db"
+# These are unnecessary and increase tarball size:
+rm -rf longdep1
+rm -rf longdep2
+rm -rf longdep3
 vname="`git describe --tags`"
 rm -rf ../.git
 rm -rf abce/.git
