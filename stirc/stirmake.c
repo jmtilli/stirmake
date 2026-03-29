bb65dccb72f9f22a43ed0f9362e0a73870fd6f22
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2026-03-29 19:20:26 +0300

    Support older POSIX too

--- stirc/stirmake.c
+++ stirc/stirmake.c
@@ -180,20 +180,35 @@ static inline struct timespec mtim_from_statbuf(struct stat *sb)
 #if defined(__APPLE__) || defined(__NetBSD__)
 #undef st_mtim_posix
 #define st_mtim_posix st_mtimespec
+#ifndef st_mtime
+#define st_mtime st_mtimespec.tv_sec
+#endif
 #endif
 #ifdef __FreeBSD__
 #if __FreeBSD_version < 1000000
 #undef st_mtim_posix
 #define st_mtim_posix st_mtimespec
+#ifndef st_mtime
+#define st_mtime st_mtimespec.tv_sec
+#endif
 #endif
 #endif
 #ifdef __OpenBSD__
 #if OpenBSD < 200905 // This means OpenBSD 4.5 according to some strange logic
 #undef st_mtim_posix
 #define st_mtim_posix st_mtimespec
+#ifndef st_mtime
+#define st_mtime st_mtimespec.tv_sec
 #endif
 #endif
+#endif
+#ifdef st_mtime
   return sb->st_mtim_posix;
+#else
+  struct timespec ts;
+  ts.tv_nsec = 0;
+  ts.tv_sec = sb->st_mtime;
+#endif
 }
 
 int silent = 0;
